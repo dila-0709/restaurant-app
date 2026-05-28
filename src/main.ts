14d8f9924328f79767ff2ac010a5457d43457ed5
gesyadila <gesyadila@gmail.com>
2026-05-28 19:41:56 +0700

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const port = process.env.PORT || 3000;
+  const config = new DocumentBuilder()
+    .setTitle('Restaurant API')
+    .setDescription('API Documentation')
+    .setVersion('1.0')
+    .build();
 
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
 
-  console.log(`Server running on port ${port}`);
+  console.log(`APP RUNNING ON PORT: ${port}`);
 }
 
 bootstrap();
