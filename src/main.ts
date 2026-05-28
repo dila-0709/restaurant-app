2f75f7e04b3485d325eaac3a76a0b167caf6d1aa
gesyadila <gesyadila@gmail.com>
2026-05-28 17:59:08 +0700

    fix railway port

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // CORS
-  app.enableCors({
-    origin: '*',
-  });
-
-  // VALIDATION
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-    }),
-  );
-
-  // SWAGGER
-  const config = new DocumentBuilder()
-    .setTitle('Restaurant API')
-    .setDescription('Backend API Sistem Restaurant')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
-  // 🔥 PORT RAILWAY WAJIB DI SINI
   const port = process.env.PORT || 3000;
 
   await app.listen(port, '0.0.0.0');
