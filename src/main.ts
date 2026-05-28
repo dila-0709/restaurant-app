fef8b6d14b005ff83c99f1ad2e1e15cd91cd8558
gesyadila <gesyadila@gmail.com>
2026-05-28 16:43:38 +0700

    fix railway expose port

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
 
   await app.listen(port, '0.0.0.0');
 
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on ${port}`);
 }
 bootstrap();
