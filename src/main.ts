f407e0247c2a34a16da0ad7954f4fb7ece71b920
gesyadila <gesyadila@gmail.com>
2026-05-28 16:57:49 +0700

    fix railway final port

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  const config = new DocumentBuilder()
-    .setTitle('Restaurant API')
-    .setDescription('API Restaurant App')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
-  const port = Number(process.env.PORT) || 3000;
-
-  await app.listen(port, '0.0.0.0');
-
-  console.log(`Server running on ${port}`);
+const app = await NestFactory.create(AppModule);
+app.useGlobalPipes(new ValidationPipe());
+const config = new DocumentBuilder()
+.setTitle('Library API')
+.setDescription('Backend API Sistem Perpustakaan')
+.setVersion('1.0')
+.addBearerAuth()
+.build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
+await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
