d9084cb5dbade6b17faa563c31a5eff7a9e9c6e4
gesyadila <gesyadila@gmail.com>
2026-05-29 14:06:36 +0700

    fix jwt strategy and auth guard

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,22 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor() {
-    super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET || 'SECRET_KEY',
-    });
-  }
-
-  async validate(payload: any) {
-    return {
-      id: payload.id,
-      email: payload.email,
-      role: payload.role,
-    };
-  }
-}
+export class JwtAuthGuard extends AuthGuard('jwt') {}
